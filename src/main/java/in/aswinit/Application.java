017c159391cdf5471088491549bf71c56cb786bd
aswinsure878787 <aswin.sure87@gmail.com>
2021-12-09 17:35:42 +0530

    commit

--- src/main/java/in/aswinit/Application.java
+++ src/main/java/in/aswinit/Application.java
@@ -8,12 +8,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-<<<<<<< HEAD
+
 
 		int b=234;
-=======
+
 		int a=123;
->>>>>>> 16c3987a64ded2649e33c75352d7e9064ce1e5d9
+
 	}
 
 }
